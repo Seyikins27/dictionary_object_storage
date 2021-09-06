68973809098223fca549c75802e02202ee21288e
unknown <solademitimileyin@gmail.com>
2021-09-06 10:45:45 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# dictionary_object_storage" 
---
+++ core/odef/create.py
@@ -0,0 +1,16 @@
+class Create:
+    username=""
+    password=""
+    conf_file="../../bin/config/global.pkl"
+
+    def __init__(self):
+        return
+
+    def start(self):
+        return
+
+    def conf_file_exists(self):
+        return
+
+    def user_exists(self):
+        return
